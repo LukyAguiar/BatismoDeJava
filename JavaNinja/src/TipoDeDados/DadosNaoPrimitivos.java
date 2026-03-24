461733e5227d2bf660b4497565f50643b8a19c05
Aguiar <lucas.nune@outlook.com.br>
2026-03-24 19:51:26 -0300

    Att project - early

---
+++ JavaNinja/src/TipoDeDados/DadosNaoPrimitivos.java
@@ -0,0 +1,18 @@
+package TipoDeDados;
+
+import java.util.Locale;
+
+public class DadosNaoPrimitivos {
+    public static void main(String[] args) {
+
+        /*
+        Dados não primitivos: String, Array, Class, Enum
+        Objetivo: Criar um ninja e atribuir métodos a ele
+         */
+
+        String nome = "Naruto";
+        String nomeUpperCase  = nome.toUpperCase();// vai colocar tuido em CAPSLOCK
+        System.out.println("texto em CAPSLOCK" + nomeUpperCase);
+        
+    }
+}
---
+++ JavaNinja/src/TipoDeDados/DadosPrimitivos.java
@@ -0,0 +1,19 @@
+package TipoDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*
+         */
+
+        int idade = 16;// Valor Maximo: 2 147 483 647
+        double altura = 1.79;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long  saldoBancario = 99999L; // Valor maximo 9,223,372,036,854,775,807
+
+        System.out.println(idade);
+        System.out.println(saldoBancario);
+        System.out.println("saldo Bancario é = " + saldoBancario);
+        System.out.println("Minha idade é: " + idade);
+    }
+}
