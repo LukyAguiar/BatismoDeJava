d0f93606208fa4fbf44528a66911b21dd1bc2f96
Aguiar <lucas.nune@outlook.com.br>
2026-03-28 13:19:57 -0300

    Aula um intermediario - completo

---
+++ JavaNinja/src/NivelIntermediario/Main.java
@@ -0,0 +1,18 @@
+package NivelIntermediario;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Ninja Naruto = new Ninja();
+
+        Naruto.setAldeia("Konoha");
+        Naruto.setIdade(15);
+        Naruto.setNome("Naruto");
+
+        Ninja Sasuke = new Ninja();
+        Sasuke.setNome("Sasuke");
+        Sasuke.setAldeia("Konoha");
+        Sasuke.setIdade(15);
+        
+    }
+}
---
+++ JavaNinja/src/NivelIntermediario/Ninja.java
@@ -0,0 +1,32 @@
+package NivelIntermediario;
+
+public class Ninja {
+
+    String nome;
+    String aldeia;
+    int idade;
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getAldeia(){
+        return aldeia;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setAldeia(String aldeia){
+        this.aldeia = aldeia;
+    }
+}
